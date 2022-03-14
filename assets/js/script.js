4819951ec75ebf3901543ad3d9a35199d22b7302
Rachel Gray <rachel.gray7448@gmail.com>
2022-03-13 20:14:24 -0400

    added local storage retrieval

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,16 @@ var currentTime = now.hour();
 // function for time blocks
 
 timeBlocks = function () {
+    $("#9").val(localStorage.getItem("9"));
     $("#10").val(localStorage.getItem("10"));
+    $("#11").val(localStorage.getItem("11"));
+    $("#12").val(localStorage.getItem("12"));
+    $("#1").val(localStorage.getItem("1"));
+    $("#2").val(localStorage.getItem("2"));
+    $("#3").val(localStorage.getItem("3"));
+    $("#4").val(localStorage.getItem("4"));
+    $("#5").val(localStorage.getItem("5"));
+
     for (let i = 0; i < $(".time-block").length; i++) {
         var time = ($(".time-block")[i].id.split("-")[1])
         if (time < currentTime) {
